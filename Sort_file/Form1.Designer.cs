3af805605ab75dbab44aef325c68495185e79f24
AtstecP <109477937+AtstecP@users.noreply.github.com>
2023-01-29 17:43:57 +0500

    first

---
+++ Sort_file/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Sort_file
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(237, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать папку";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(272, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(272, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Имя папки с результатом";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(538, 140);
+            this.label2.TabIndex = 8;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox1.ImageKey = "(none)";
+            this.checkBox1.Location = new System.Drawing.Point(272, 197);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.checkBox1.Size = new System.Drawing.Size(377, 25);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.TabStop = false;
+            this.checkBox1.Text = "Создавать папку с рузльтатом на рабочем столе";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(272, 144);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(240, 29);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(272, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 21);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Ограничение на количество файлов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(795, 510);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "che";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        public CheckBox checkBox1;
+        private TextBox textBox2;
+        private Label label3;
+    }
+}
---
+++ Sort_file/Form1.cs
@@ -0,0 +1,118 @@
+using System.Diagnostics;
+
+namespace Sort_file
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            string date = Convert.ToString(DateTime.Today).Substring(0, 10);
+            InitializeComponent();
+            AutoCompleteStringCollection source = new AutoCompleteStringCollection()
+            {
+                date,
+                "Отсортированное",
+            };
+            textBox1.AutoCompleteCustomSource = source;
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            textBox1.Text = date;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                //MessageBox.Show(FBD.SelectedPath);
+				Sort_Files(FBD.SelectedPath);
+            }
+        }
+
+		private void Sort_Files(string path)
+		{
+            int amount = Convert.ToInt32(textBox2.Text);
+			string[] files = Directory.GetFiles(path, "*_*_(*", SearchOption.AllDirectories);
+            if (checkBox1.Checked == true)
+                path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+			for (int i = 0; i < files.Length; i++)
+			{
+				string file_name = files[i];
+                string path_local = Path.Combine(path, textBox1.Text);
+				string fileName = Path.GetFileName(file_name);
+				string text = file_name.Substring(file_name.IndexOf("_(") - 3, 3);
+				if (text.IndexOf("_") != -1)
+				{
+					text = text.Substring(text.IndexOf("_") + 1);
+				}
+
+				path_local = Path.Combine(path_local, text);
+				if (!Directory.Exists(path_local))
+				{ 
+					Directory.CreateDirectory(path_local);
+				}
+                else if (amount > 0)
+                {
+                    path_local = check_amount(path_local, amount);
+                }
+
+                path_local = Path.Combine(path_local, fileName);
+                if (!File.Exists(path_local))
+                    File.Copy(file_name, path_local);
+			}
+            open_folder(Path.Combine(path, textBox1.Text));
+        }
+
+        private string check_amount(string path, int amount, int tag = 1)
+        {
+            if (Directory.GetFiles(path).Length >= amount)
+            {
+                if (tag != 1)
+                    path = path.Replace($"_{tag-1}", $"_{tag}");
+                else
+                    path += $"_{tag}";
+
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                else
+                    path = check_amount(path, amount, tag+1);
+            }
+
+            return path;
+        }
+
+        private void open_folder(string path)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = path,
+                UseShellExecute = true
+            }
+            );
+        }
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
